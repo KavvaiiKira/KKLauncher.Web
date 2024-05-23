fc0cae4c225c666507e5ccafa0244aeace56f294
MUaRka <lexminer2014@gmail.com>
2024-05-23 18:36:03 +0300

    Removed files

--- KKLauncher.DB/Entities/AppCollectionEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KKLauncher.DB.Entities
-{
-    public class AppCollectionEntity
-    {
-        public Guid AppId { get; set; }
-
-        public Guid CollectionId { get; set; }
-    }
-}
--- KKLauncher.DB/Entities/WhitelistEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KKLauncher.DB.Entities
-{
-    public class WhitelistEntity
-    {
-        public Guid Id { get; set; }
-
-        public string TelegramId { get; set; }
-    }
-}
